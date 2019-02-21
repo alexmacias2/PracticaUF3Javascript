d4b4644d9a3706c36d24c4e38cc43ef76fa38ad6
David Ortiz <cmortizdavid@gmail.com>
2019-02-21 20:35:33 +0100

    Botón modificar y separación de código.

--- public_html/js/myscript.js
+++ public_html/js/myscript.js
@@ -76,6 +76,11 @@ function paginaCargada() {
             document.getElementById(idBoton[0] + '-' + 'cantidad').style.display = 'none';
             document.getElementById(this.id).style.display = 'none';
 
+            opcionesElementos(cantidad, idBoton);
+
+        }
+
+        function opcionesElementos(cantidad, idBoton) {
             for (var i = 0; i < cantidad; i++) {
                 var select = document.createElement('select');
                 select.setAttribute('id', idBoton[0] + '-select' + i);
@@ -107,147 +112,247 @@ function paginaCargada() {
                 form.appendChild(select);
             }
         }
-
         function diseñaCampo() {
             var saltoLinea = document.createElement("br");
 
             switch (document.getElementById(this.id).value) {
                 case 'button':
                     form.removeChild(document.getElementById(this.id));
-                    var textoBtn = prompt("Inserta texto del botón", "");
-                    var boton = document.createElement('button');
-                    boton.setAttribute("id", tituloFormulario.innerHTML + "-btn1");
-                    boton.setAttribute("type", "button");
-                    boton.appendChild(document.createTextNode(textoBtn));
+                    crearBoton();
+
                     var btnBorrarElemento = document.createElement('button');
-                    btnBorrarElemento.addEventListener('click', function () {
-                        form.removeChild(boton);
-                        btnBorrarElemento.style.display = 'none';
-                    });
                     btnBorrarElemento.setAttribute('type', 'button');
                     btnBorrarElemento.appendChild(document.createTextNode('Borrar'));
+
+                    var btnModificarElemento = document.createElement('button');
+                    btnModificarElemento.setAttribute('type', 'button');
+                    btnModificarElemento.appendChild(document.createTextNode('Modificar'));
+
+                    btnBorrarElemento.addEventListener('click', function () {
+                        form.removeChild(boton);
+                        form.removeChild(btnModificarElemento);
+                        form.removeChild(btnBorrarElemento);
+                        form.removeChild(saltoLinea);
+                    });
+                    btnModificarElemento.addEventListener('click', function () {
+                        form.removeChild(boton);
+                        form.removeChild(btnBorrarElemento);
+                        form.removeChild(btnModificarElemento);
+                        form.removeChild(saltoLinea);
+                        opcionesElementos(1, this.id.split('-', 2));
+                    });
+
                     form.appendChild(saltoLinea);
-                    form.appendChild(boton);
                     form.appendChild(btnBorrarElemento);
+                    form.appendChild(btnModificarElemento);
 
                     break;
                 case 'input':
                     form.removeChild(document.getElementById(this.id));
-                    var input = document.createElement("input");
-                    input.setAttribute("id", tituloFormulario.innerHTML + "-input");
-                    input.setAttribute("placeholder", "texto por defecto");
+                    crearInput();
+
                     var btnBorrarElemento = document.createElement('button');
-                    btnBorrarElemento.addEventListener('click', function () {
-                        form.removeChild(input);
-                        btnBorrarElemento.style.display = 'none';
-                    });
                     btnBorrarElemento.setAttribute('type', 'button');
                     btnBorrarElemento.appendChild(document.createTextNode('Borrar'));
+
+                    var btnModificarElemento = document.createElement('button');
+                    btnModificarElemento.setAttribute('type', 'button');
+                    btnModificarElemento.appendChild(document.createTextNode('Modificar'));
+
+                    btnBorrarElemento.addEventListener('click', function () {
+                        form.removeChild(input);
+                        form.removeChild(btnModificarElemento);
+                        form.removeChild(btnBorrarElemento);
+                        form.removeChild(saltoLinea);
+                    });
+                    btnModificarElemento.addEventListener('click', function () {
+                        form.removeChild(input);
+                        form.removeChild(btnBorrarElemento);
+                        form.removeChild(btnModificarElemento);
+                        form.removeChild(saltoLinea);
+                        opcionesElementos(1, this.id.split('-', 2));
+                    });
+
                     form.appendChild(saltoLinea);
-                    form.appendChild(input);
                     form.appendChild(btnBorrarElemento);
+                    form.appendChild(btnModificarElemento);
                     break;
                 case 'Selection':
                     form.removeChild(document.getElementById(this.id));
-                    var opciones = prompt('Cuantas opciones necesitas?', '');
-                    var select = document.createElement('select');
-                    for (var i = 0; i < parseInt(opciones); i++) {
-                        var textoOpcion = prompt('Introduce opcion:', '');
-
-                        var opcion = document.createElement('option');
-                        opcion.appendChild(document.createTextNode(textoOpcion));
-                        select.appendChild(opcion);
-                    }
+                    crearSelection();
                     var btnBorrarElemento = document.createElement('button');
-                    btnBorrarElemento.addEventListener('click', function () {
-                        form.removeChild(select);
-                        btnBorrarElemento.style.display = 'none';
-                    });
                     btnBorrarElemento.setAttribute('type', 'button');
                     btnBorrarElemento.appendChild(document.createTextNode('Borrar'));
+
+                    var btnModificarElemento = document.createElement('button');
+                    btnModificarElemento.setAttribute('type', 'button');
+                    btnModificarElemento.appendChild(document.createTextNode('Modificar'));
+
+                    btnBorrarElemento.addEventListener('click', function () {
+                        form.removeChild(select);
+                        form.removeChild(btnModificarElemento);
+                        form.removeChild(btnBorrarElemento);
+                        form.removeChild(saltoLinea);
+                    });
+                    btnModificarElemento.addEventListener('click', function () {
+                        form.removeChild(select);
+                        form.removeChild(btnBorrarElemento);
+                        form.removeChild(btnModificarElemento);
+                        form.removeChild(saltoLinea);
+                        opcionesElementos(1, this.id.split('-', 2));
+                    });
                     form.appendChild(saltoLinea);
-                    form.appendChild(select);
                     form.appendChild(btnBorrarElemento);
+                    form.appendChild(btnModificarElemento);
                     break;
                 case 'checkBox':
                     form.removeChild(document.getElementById(this.id));
-                    var cantidad = prompt("Inserta cantidad de checks");
-                    var div = document.createElement("div");
-                    for (var i = 0; i < parseInt(cantidad); i++) {
-                        var check = document.createElement("input");
-                        check.setAttribute("type", "checkbox");
-                        div.appendChild(check);
-                        var textoCheck = prompt("inserta texto1", "");
-                        var span = document.createElement("span");
-                        span.appendChild(document.createTextNode(textoCheck));
-                        div.appendChild(span);
-                        var saltoLinea = document.createElement("br");
-                        div.appendChild(saltoLinea);
-                    }
+                    crearChecks();
                     var btnBorrarElemento = document.createElement('button');
-                    btnBorrarElemento.addEventListener('click', function () {
-                        form.removeChild(div);
-                        btnBorrarElemento.style.display = 'none';
-                    });
                     btnBorrarElemento.setAttribute('type', 'button');
                     btnBorrarElemento.appendChild(document.createTextNode('Borrar'));
-                    div.setAttribute("id", tituloFormulario.innerHTML + "-" + "divCheck");
+
+                    var btnModificarElemento = document.createElement('button');
+                    btnModificarElemento.setAttribute('type', 'button');
+                    btnModificarElemento.appendChild(document.createTextNode('Modificar'));
+
+                    btnBorrarElemento.addEventListener('click', function () {
+                        form.removeChild(divCheck);
+                        form.removeChild(btnModificarElemento);
+                        form.removeChild(btnBorrarElemento);
+                        form.removeChild(saltoLinea);
+                    });
+                    btnModificarElemento.addEventListener('click', function () {
+                        form.removeChild(divCheck);
+                        form.removeChild(btnBorrarElemento);
+                        form.removeChild(btnModificarElemento);
+                        form.removeChild(saltoLinea);
+                        opcionesElementos(1, this.id.split('-', 2));
+                    });
                     form.appendChild(saltoLinea);
-                    form.appendChild(div);
                     form.appendChild(btnBorrarElemento);
+                    form.appendChild(btnModificarElemento);
                     break;
                 case 'RadioButton':
                     form.removeChild(document.getElementById(this.id));
-                    var cantidad = prompt("Inserta cantidad de radio button");
-                    var div = document.createElement("div");
-                    for (var i = 0; i < parseInt(cantidad); i++) {
-                        var radio = document.createElement("input");
-                        radio.setAttribute("type", "radio");
-                        radio.setAttribute("name", "prueba");
-                        div.appendChild(radio);
-                        var textoRadio = prompt("inserta texto1", "");
-                        var span = document.createElement("span");
-                        span.appendChild(document.createTextNode(textoRadio));
-                        div.appendChild(span);
-                        var saltoLinea = document.createElement("br");
-                        div.appendChild(saltoLinea);
-                    }
+                    crearRadios();
                     var btnBorrarElemento = document.createElement('button');
-                    btnBorrarElemento.addEventListener('click', function () {
-                        form.removeChild(div);
-                        btnBorrarElemento.style.display = 'none';
-                    });
                     btnBorrarElemento.setAttribute('type', 'button');
                     btnBorrarElemento.appendChild(document.createTextNode('Borrar'));
-                    div.setAttribute("id", tituloFormulario.innerHTML + "-" + "divCheck");
-                    div.setAttribute("id", tituloFormulario.innerHTML + "-" + "divRadio");
-                    form.appendChild(div);
+
+                    var btnModificarElemento = document.createElement('button');
+                    btnModificarElemento.setAttribute('type', 'button');
+                    btnModificarElemento.appendChild(document.createTextNode('Modificar'));
+
+                    btnBorrarElemento.addEventListener('click', function () {
+                        form.removeChild(divRadio);
+                        form.removeChild(btnModificarElemento);
+                        form.removeChild(btnBorrarElemento);
+                        form.removeChild(saltoLinea);
+                    });
+                    btnModificarElemento.addEventListener('click', function () {
+                        form.removeChild(divRadio);
+                        form.removeChild(btnBorrarElemento);
+                        form.removeChild(btnModificarElemento);
+                        form.removeChild(saltoLinea);
+                        opcionesElementos(1, this.id.split('-', 2));
+                    });
+                    form.appendChild(saltoLinea);
                     form.appendChild(btnBorrarElemento);
+                    form.appendChild(btnModificarElemento);
                     break;
             }
 
-        }
-        
-        function compruebaNombre(nombreFormulario) {
-        var nombreFormulario = nombreFormulario;
-        var existe = false;
-
-        for (var i = 0; i < nombresFormularios.length; i++) {
-            if (nombresFormularios[i] == nombreFormulario) {
-                existe = true;
+            function crearBoton() {
+                var textoBtn = prompt("Inserta texto del botón", "");
+                boton = document.createElement('button');
+                boton.setAttribute("id", tituloFormulario.innerHTML + "-btn1");
+                boton.setAttribute("type", "button");
+                boton.appendChild(document.createTextNode(textoBtn));
+                form.appendChild(boton);
             }
+            function crearInput() {
+                input = document.createElement("input");
+                input.setAttribute("id", tituloFormulario.innerHTML + "-input");
+                input.setAttribute("placeholder", "texto por defecto");
+                
+                form.appendChild(input);
+            }
+
+            function crearSelection() {
+                var opciones = prompt('Cuantas opciones necesitas?', '');
+                select = document.createElement('select');
+                for (var i = 0; i < parseInt(opciones); i++) {
+                    var textoOpcion = prompt('Introduce opcion:', '');
+
+                    var opcion = document.createElement('option');
+                    opcion.appendChild(document.createTextNode(textoOpcion));
+                    select.appendChild(opcion);
+                }
+                form.appendChild(select);
+            }
+
+            function crearChecks() {
+                var cantidad = prompt("Inserta cantidad de checks");
+                divCheck = document.createElement("div");
+                divCheck.setAttribute("id", tituloFormulario.innerHTML + "-" + "divCheck");
+                for (var i = 0; i < parseInt(cantidad); i++) {
+                    var check = document.createElement("input");
+                    check.setAttribute("type", "checkbox");
+                    divCheck.appendChild(check);
+                    var textoCheck = prompt("inserta texto " + (i + 1), "");
+                    var span = document.createElement("span");
+                    span.appendChild(document.createTextNode(textoCheck));
+                    divCheck.appendChild(span);
+                    var saltoLinea = document.createElement("br");
+                    divCheck.appendChild(saltoLinea);
+                }
+                form.appendChild(divCheck);
+            }
+
+            function crearRadios() {
+                var cantidad = prompt("Inserta cantidad de radio button");
+                divRadio = document.createElement("div");
+                divRadio.setAttribute("id", tituloFormulario.innerHTML + "-" + "divRadio");
+                for (var i = 0; i < parseInt(cantidad); i++) {
+                    var radio = document.createElement("input");
+                    radio.setAttribute("type", "radio");
+                    radio.setAttribute("name", "prueba");
+                    divRadio.appendChild(radio);
+                    var textoRadio = prompt("inserta texto " + (i + 1), "");
+                    var span = document.createElement("span");
+                    span.appendChild(document.createTextNode(textoRadio));
+                    divRadio.appendChild(span);
+                    var saltoLinea = document.createElement("br");
+                    divRadio.appendChild(saltoLinea);
+                }
+
+                form.appendChild(divRadio);
+            }
+
+
         }
-        
-        if (!existe) {
-            nombresFormularios.push(nombreFormulario);
+
+        function compruebaNombre(nombreFormulario) {
+            var nombreFormulario = nombreFormulario;
+            var existe = false;
+
+            for (var i = 0; i < nombresFormularios.length; i++) {
+                if (nombresFormularios[i] == nombreFormulario) {
+                    existe = true;
+                }
+            }
+
+            if (!existe) {
+                nombresFormularios.push(nombreFormulario);
+            }
+
+            return existe;
         }
-        
-        return existe;
-    }
 
     }
 
-    
-    
-   
+
+
+
 }
